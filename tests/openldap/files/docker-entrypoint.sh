cfafea765b71edc6c98ab2f6f1804b2eb6248045
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-06-03 16:56:12 +0200

    🐳 fix ldap configuration + add basic template

--- tests/openldap/files/docker-entrypoint.sh
+++ tests/openldap/files/docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -ex
 
-/usr/sbin/slapd -d3 -s trace -h "ldap://0.0.0.0:389/ ldapi://%2Fvar%2Frun%2Fslapd%2Fldapi ldaps://0.0.0.0:636/" -f /etc/ldap/slapd.conf
+# Remove old configuration
+rm -rf /etc/ldap/slapd.d/* || true
+
+# Import templates
+slapadd -v -n 1 -l /templates/ldap.ldif
+
+/usr/sbin/slapd -d3 -s trace -f /etc/ldap/slapd.conf
