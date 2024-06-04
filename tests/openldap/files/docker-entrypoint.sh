5086fde17ed6bf892b6e8cc0b34a033a534ef68e
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-06-04 11:25:54 +0200

    🐳 remove ldap files at launch (clean start)

--- tests/openldap/files/docker-entrypoint.sh
+++ tests/openldap/files/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 
 # Remove old configuration
-rm -rf /etc/ldap/slapd.d/* || true
+rm -rf /var/lib/ldap/* || true
 
 # Import templates
-slapadd -v -n 1 -l /templates/ldap.ldif
+slapadd -v -n 1 -l /templates/ldap.ldif -f /etc/ldap/slapd.conf
 
+# Start slapd
 /usr/sbin/slapd -d3 -s trace -f /etc/ldap/slapd.conf
